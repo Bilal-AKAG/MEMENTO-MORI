dc15742092671e937099af3060d2356578d7e756
Bilal Ali <bilal.ali.irp.dev@gmail.com>
2025-12-01 08:53:17 +0300

    chnage favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -35,6 +35,9 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <HeadContent />
       </head>
       <body className={cn("antialiased bg-terminal-black ")}>
