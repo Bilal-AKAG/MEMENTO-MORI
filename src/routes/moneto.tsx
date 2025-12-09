fbb573b335a1c42b8405a236dd98aa6211df2b86
Bilal Ali <bilal.ali.irp.dev@gmail.com>
2025-12-09 15:22:52 +0300

    Update README

--- src/routes/moneto.tsx
+++ src/routes/moneto.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import React, { useState, useEffect, useMemo } from "react";
+import { useState, useEffect } from "react";
 import { ActivityStats, LifeData, CountryLifeExpectancy } from "../types";
 import {
   COUNTRY_DATA,
